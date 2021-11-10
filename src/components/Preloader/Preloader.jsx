d74c76bcac4324ab060076a00e66b45cd4f6f68c
myr-irina <myr-irina2@yandex.ru>
2021-11-10 19:03:28 +0300

    code clean up

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -2,17 +2,11 @@ import React from "react";
 import PreloaderCard from "../PreloaderCard/PreloaderCard";
 import "./Preloader.css";
 
-
 export default function Preloader() {
   return (
     <div>
       <div className="preloader page__content">
         <ul className="preloader__list">
-          {/* <li className="preloader__round"></li>
-          <div className="preloader__rectangle-block">
-            <li className="preloader__rectangle"></li>
-            <li className="preloader__rectangle-small"></li>
-          </div> */}
           <PreloaderCard />
           <PreloaderCard />
           <PreloaderCard />
--- src/components/PreloaderCard/PreloaderCard.jsx
+++ src/components/PreloaderCard/PreloaderCard.jsx
@@ -8,7 +8,6 @@ export default function PreloaderCard() {
       <div className="preloader-card-block">
         <div className="preloader-card-block__name-details">
           <div className="preloader-card-block__name"></div>
-       
         </div>
         <div className="preloader-card-block__role"></div>
       </div>
